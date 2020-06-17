ab1dca91a4dceb4554ea3d668a325172063a833a
IndominusByte <nyomanpradipta120@gmail.com>
2020-06-17 23:58:55 +0800

    update config

--- restapi/services/config.py
+++ restapi/services/config.py
@@ -6,8 +6,7 @@ class Config:
     DEBUG = False
     SECRET_KEY = os.getenv('SECRET_KEY')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir,'data.db')
-    SQLALCHEMY_DATABASE_URI = 'postgresql:///balihotproperty'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir,'data.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_ACCESS_TOKEN_EXPIRES = 15 * 60  # 15 minute
     JWT_REFRESH_TOKEN_EXPIRES = 30 * 86400  # 30 days
@@ -16,6 +15,7 @@ class Config:
 
 class Development(Config):
     DEBUG = True
+    # SQLALCHEMY_DATABASE_URI = 'postgresql:///balihotproperty'
 
 class Production(Config):
     DEBUG = False
