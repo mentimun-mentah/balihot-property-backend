54ac8ffc88e5d3cd61608b2101eb8a971e6359cc
IndominusByte <nyomanpradipta120@gmail.com>
2020-06-29 21:33:51 +0800

    delete crud type

--- migrate.sh
+++ migrate.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 docker-compose exec backend /bin/bash -c "flask db init; flask db migrate -m 'init db'; flask db upgrade"
+docker-compose exec postgres psql -c "INSERT INTO types (name) VALUES ('Villa'),('Land')" -U balihotproperty
